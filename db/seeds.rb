9b5f6726569f80af81fe5e1c7cc80c57e7c4808b
Wesley Carr <surfswell17@gmail.com>
2013-07-16 10:14:43 -0400

    updating gemfile for graphviz examples

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 House.delete_all
-House.create(:name => "Slytherin")
-House.create(:name => "Ravenclaw")
-House.create(:name => "Hufflepuff")
-House.create(:name => "Gryffindor")
+Student.delete_all
+
+h1 = House.create(:name => "Gryffindor")
+h2 = House.create(:name => "Slytherin")
+h3 = House.create(:name => "Ravenclaw")
+h4 = House.create(:name => "Hufflepuff")
+
+
+s1 = Student.create(:name => "Harry Potter")
+s2 = Student.create(:name => "Ron Weasley")
+s3 = Student.create(:name => "Hermoine Granger")
+s4 = Student.create(:name => "Draco Malfoy")
+s5 = Student.create(:name => "Luna Lovejoy")
+
+h1.students << [s1, s2, s3]
+h2.students << s4
+h4.students << s5
