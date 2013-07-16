9a76f4e2165c128e6ae378c92abd85af68a9d82e
Wesley Carr <surfswell17@gmail.com>
2013-07-16 10:31:01 -0400

    Tested annotate and graphviz

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  house_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Student < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :house_id, :spell_id
   belongs_to :house
   has_and_belongs_to_many :spells
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,49 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 House.delete_all
 Student.delete_all
+Spell.delete_all
 
-h1 = House.create(:name => "Gryffindor")
-h2 = House.create(:name => "Slytherin")
-h3 = House.create(:name => "Ravenclaw")
-h4 = House.create(:name => "Hufflepuff")
+h1 = House.create(name: "Gryffindor")
+h2 = House.create(name: "Slytherin")
+h3 = House.create(name: "Ravenclaw")
+h4 = House.create(name: "Hufflepuff")
 
+s1 = Student.create(name: "Harry Potter")
+s2 = Student.create(name: "Ron Weasley")
+s3 = Student.create(name: "Hermione Granger")
 
-s1 = Student.create(:name => "Harry Potter")
-s2 = Student.create(:name => "Ron Weasley")
-s3 = Student.create(:name => "Hermoine Granger")
-s4 = Student.create(:name => "Draco Malfoy")
-s5 = Student.create(:name => "Luna Lovejoy")
+s4 = Student.create(name: "Draco Malfoy")
+s5 = Student.create(name: "Vincent Crabbe")
+s6 = Student.create(name: "Gregory Goyle")
 
-h1.students << [s1, s2, s3]
-h2.students << s4
-h4.students << s5
+s7 = Student.create(name: "Luna Lovegood")
+s8 = Student.create(name: "Cho Chang")
+s9 = Student.create(name: "Padma Patil")
+
+s10 = Student.create(name: "Cedric Diggory")
+s11 = Student.create(name: "Zacharias Smith")
+s12 = Student.create(name: "Justin Finch-Fletchley")
+
+h1.students = [s1, s2, s3]
+h2.students = [s4, s5, s6]
+h3.students = [s7, s8, s9]
+h4.students = [s10, s11, s12]
+
+sp1 = Spell.create(name: "Accio")
+sp2 = Spell.create(name: "Alohomora")
+sp3 = Spell.create(name: "Wingardium Leviosa")
+sp4 = Spell.create(name: "Avada Kedavra")
+sp5 = Spell.create(name: "Repairo")
+sp6 = Spell.create(name: "Crucio")
+sp7 = Spell.create(name: "Riddikulus")
+sp8 = Spell.create(name: "Expulso")
+sp9 = Spell.create(name: "Expecto Patronum")
+
+Student.all.each do |student|
+  student.spells << sp1 << sp3
+end
+
+s1.spells << sp9
+s4.spells << sp4 << sp6
+
+puts "All done"
